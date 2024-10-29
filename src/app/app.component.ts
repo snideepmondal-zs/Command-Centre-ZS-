e4fd7bf61b7d98cb702ceebcc649b4e4232bebdd
Snideep Mondal <snideep.mondal@zs.com>
2024-10-29 19:51:21 +0530

    Home Screen : Left Side Navbar Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzLayoutModule, NzIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
