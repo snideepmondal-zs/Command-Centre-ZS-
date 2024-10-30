279c5c9968ce8c1015d47688c533bc6c9b5bf39f
Snideep Mondal <snideep.mondal@zs.com>
2024-10-30 08:19:30 +0530

    Home Screen Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { AccordianComponent } from './shared/accordian/accordian.component';
  
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NzLayoutModule, NzIconModule],
+  imports: [RouterOutlet, NzLayoutModule, NzIconModule, AccordianComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
