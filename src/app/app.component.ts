f56bb1c160ffd8fb1d783024627cb9e9b94dd464
Snideep Mondal <snideep.mondal@zs.com>
2024-10-30 09:57:00 +0530

    Structure Fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterLink} from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { AccordianComponent } from './shared/accordian/accordian.component';
- 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NzLayoutModule, NzIconModule, AccordianComponent],
+  imports: [RouterModule, RouterLink, RouterOutlet, NzLayoutModule, NzIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'command-centre';
 }
---
+++ src/app/shared/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { AccordianComponent } from '../accordian/accordian.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NzLayoutModule, AccordianComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
